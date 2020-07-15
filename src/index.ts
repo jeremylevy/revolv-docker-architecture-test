52b55bc9070c56458cc46bbc7c8b957fe16a3df4
Jérémy Levy <jje.levy@gmail.com>
2020-07-15 19:53:10 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 8080
 
 app.get("/", (_req, res) => {
-  res.send("Hello from Revolv!")
+  res.send("Hello from Revolv! " + process.env.ASSETS_CLOUDFRONT_URL + " - " + process.env.ASSETS_S3_BUCKET)
 })
 
 app.listen(port)
